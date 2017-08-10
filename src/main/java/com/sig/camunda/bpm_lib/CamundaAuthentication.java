0effd3505576f8f9dea45b6e7bd57bd9e14bb913
Roycer <Roycer@DESKTOP-E87A7G6>
2017-08-10 11:49:21 -0500

    readProcessDefinition

--- src/main/java/com/sig/camunda/bpm_lib/CamundaAuthentication.java
+++ src/main/java/com/sig/camunda/bpm_lib/CamundaAuthentication.java
@@ -81,12 +81,12 @@ public class CamundaAuthentication implements CamundaAuth{
 	 * @see
 	 */
 	public boolean isAuthorizationReadProcessDefinition(String user, String processDefinition){
-		 List<Group> groups = processEngine.getIdentityService().createGroupQuery().groupMember(user).list();
-		    List<String> groupIds = new ArrayList<String>();
-		    for (Group group : groups) 
-		    	groupIds.add(group.getId());
+		List<Group> groups = processEngine.getIdentityService().createGroupQuery().groupMember(user).list();
+		List<String> groupIds = new ArrayList<String>();
+		for (Group group : groups) 
+			groupIds.add(group.getId());
 		    
-			return this.authorizationService.isUserAuthorized(user, groupIds, Permissions.READ, Resources.PROCESS_DEFINITION,processDefinition) ;
+		return this.authorizationService.isUserAuthorized(user, groupIds, Permissions.READ, Resources.PROCESS_DEFINITION,processDefinition) ;
 	}
 	
 	/**
